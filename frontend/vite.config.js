336ecfd64917371e196df9bf08b99ba6c2d8d3a6
codecraft-sanju <sanjusirvich18@gmail.com>
2025-04-10 12:25:12 +0530

    haribol

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-         target:"http://localhost:5000"
-       // target:"https://instaverse-beru.onrender.com"
-      }
-    }
+      '/api': {
+        target: 'https://instaverse-i93f.onrender.com',
+        // target:"https://instaverse-beru.onrender.com"
+      },
+    },
   },
   build: {
-    chunkSizeWarningLimit: 2000
-  }
-})
+    chunkSizeWarningLimit: 2000,
+  },
+});
